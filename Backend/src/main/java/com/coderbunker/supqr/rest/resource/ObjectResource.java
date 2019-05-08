177204710bf0703ca61c33ca1e81865c74bc7a57
Noah Stalder <noah.stalder@ergon.ch>
2019-05-08 12:49:07 +0800

    danke fif

--- Backend/src/main/java/com/coderbunker/supqr/rest/resource/ObjectResource.java
+++ Backend/src/main/java/com/coderbunker/supqr/rest/resource/ObjectResource.java
@@ -2,22 +2,15 @@ package com.coderbunker.supqr.rest.resource;
 
 import java.io.ByteArrayInputStream;
 import java.util.List;
-import com.coderbunker.supqr.annotation.Registered;
-import com.coderbunker.supqr.auth.User;
-import com.coderbunker.supqr.rest.model.ObjectSummaryTO;
-import com.coderbunker.supqr.rest.model.ObjectTO;
-import com.coderbunker.supqr.service.ObjectService;
-import io.dropwizard.auth.Auth;
-import lombok.RequiredArgsConstructor;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
